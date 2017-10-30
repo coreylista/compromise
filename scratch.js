c3de20c4e796ba739cea999f04937586d1eedcd0
Corey Lista <coreylista@me.com>
2017-10-30 10:55:30 -0400

    sync

--- scratch.js
+++ scratch.js
@@ -1,7 +1,44 @@
-var nlp = require('./src/index');
-// nlp.verbose('tagger');
+const nlp = require('./src/index');
+nlp.verbose('tagger');
 
+const tagSets = {
+    Device: {},
+    Room: {},
+    DeviceType: {},
+    Command: {},
+    Status: {},
+    'aa55d3cf-acdc-4413-8b04-edf33ccef4ac': {
+        isA: 'Device'
+    }
+};
+nlp.addTags(tagSets);
 
+const words = {
+    'desk lamp': 'aa55d3cf-acdc-4413-8b04-edf33ccef4ac',
+    office: 'Room',
+    kitchen: 'Room',
+    upstairs: 'Room',
+    garage: 'Room',
+    on: 'Command',
+    off: 'Command',
+    open: 'Command',
+    close: 'Command',
+    lock: 'Command',
+    unlock: 'Command'
+};
+nlp.addWords(words);
 
-var doc = nlp('i just walked to the store');
-doc.match('just [#PastTense] to the store').debug();
+const regEx = {
+    'light(?:(?:s|es)?)': 'DeviceType'
+};
+nlp.addRegex(regEx);
+
+const patterns = {
+    '(#Device|#Command|#Value|#Unit)': 'DeviceAction',
+    '(#DeviceType|#Command|#Value|#Unit|#Room)': 'DeviceTypeAction'
+};
+nlp.addPatterns(patterns);
+
+var doc = nlp('turn desk lamp on at fifty percent an the lights on in the office');
+let action = doc.match('(#DeviceAction|#DeviceTypeAction)+');
+console.log(action.data());
